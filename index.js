f6e8bd36300965f14abf621311afbccf2f461654
benson <mwangibenson923@gmail.com>
2024-07-03 14:41:42 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply =num1*num2
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random()*10)
+
+console.log(random+0);
+
+const num3 =9
+const num4 =5
+
+const mod =num3 % num4
+
+console.log(mod);
+
+const max = Math.max(2,4,8,10,15,20)
+console.log(max);
+
